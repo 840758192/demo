e2e039468818f415207f7439084c0b0603ae11ac
lijian <840758192@qq.com>
2017-10-31 22:16:14 +0800

    上传大文件

--- src/main/java/cn/xblltech/demo/redis/MergeThred.java
+++ src/main/java/cn/xblltech/demo/redis/MergeThred.java
@@ -4,8 +4,10 @@ import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.StringUtils;
 
 import java.io.File;
+import java.io.RandomAccessFile;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -21,26 +23,30 @@ public class MergeThred implements Runnable {
     @Override
     public synchronized void run() {
         try {
-            File newFile = new File(fileMessages.getName());
+            RandomAccessFile rFile = new RandomAccessFile(fileMessages.getName(), "rw");
+//            rFile.setLength(Integer.valueOf(fileMessages.getChunkSize()) * (fileMessages.getTotalChunks().intValue() - 1)
+//                    + ((byte[])next2).length);
             long size = 0;
-            int i = 0;
+            int i = 1;
             for (; i <= fileMessages.getTotalChunks(); i++) {
                 Set<Object> objects = redisTemplate.boundZSetOps(fileMessages.getIdentifier()).rangeByScore(i, i);
-                if (objects != null) {
+                if (!objects.isEmpty()) {
                     redisTemplate.boundZSetOps(fileMessages.getIdentifier()).removeRangeByScore(i, i);
                     Iterator<Object> iterator = objects.iterator();
                     while (iterator.hasNext()) {
                         Object next = iterator.next();
                         if (next instanceof byte[]) {
-                            FileUtils.writeByteArrayToFile(newFile, (byte[]) next, true);
+                            rFile.seek(size);
+                            rFile.write((byte[])next);
                             size += ((byte[]) next).length;
                             LOGGER.info("文件大小：" + size + ",每块数据大小:" + ((byte[]) next).length + ",文件块数:" + i);
                         }
                     }
                 } else {
-                    i = i - 1;
+                    i--;
                 }
             }
+            rFile.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
